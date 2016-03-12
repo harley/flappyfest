c3b5dc3438c3d5792760a80839369b265bcbb091
Harley Trung <harley@harley.io>
2016-03-12 13:48:42 +0700

    milestone 5

--- flappy2/ViewController.swift
+++ flappy2/ViewController.swift
@@ -10,24 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     var animator: UIDynamicAnimator!
-    var gravity = UIGravityBehavior()
+    var gravity: UIGravityBehavior!
+    var push: UIPushBehavior!
+    var birdProperties: UIDynamicItemBehavior!
+    
     var isPlaying = false
     var birdOrigin: CGPoint!
     
     @IBAction func onPlayTapped(sender: UIButton) {
         if isPlaying {
-            // STOP
+            print("STOPPING")
+            sender.setTitle("Play", forState: .Normal)
             isPlaying = false
             birdView.frame.origin = birdOrigin
             animator.removeAllBehaviors()
         } else {
-            // PLAY
+            print("PLAYING")
+            sender.setTitle("Stop", forState: .Normal)
             isPlaying = true
-            // store bird position
-            birdOrigin = birdView.frame.origin
             setupBehaviors()
         }
-        print("play is tapped")
     }
     
     @IBOutlet weak var birdView: UIImageView!
@@ -35,20 +37,37 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // store bird position
+        birdOrigin = birdView.frame.origin
         
         animator = UIDynamicAnimator(referenceView: view)
-        gravity.addItem(birdView)
+        gravity = UIGravityBehavior(items: [birdView])
+        push = UIPushBehavior(items: [birdView], mode: UIPushBehaviorMode.Instantaneous)
+        push.active = false
+        push.pushDirection = CGVectorMake(0, -1.1)
+        
+        birdProperties = UIDynamicItemBehavior(items: [birdView])
+    }
+    
+    @IBAction func onScreenTap(sender: UITapGestureRecognizer) {
+        print("tapping on screen")
+        
+        let currentVelocity = birdProperties.linearVelocityForItem(birdView)
+        print("current velocity", currentVelocity)
+        birdProperties.addLinearVelocity(CGPoint(x: currentVelocity.x, y: -currentVelocity.y), forItem: birdView)
+        
+        push.active = true
+        animator.addBehavior(push)
     }
     
     func setupBehaviors() {
+        // we will add more behaviors soon
         animator.addBehavior(gravity)
+        animator.addBehavior(birdProperties)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
 }
-
