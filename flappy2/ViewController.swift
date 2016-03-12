86e6f723c0b143d0e2f893b72d2afd2322caf212
Harley Trung <harley@harley.io>
2016-03-12 12:42:19 +0700

    Initial Commit

---
+++ flappy2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  flappy2
+//
+//  Created by Harley Trung on 3/12/16.
+//  Copyright © 2016 coderschool.vn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
